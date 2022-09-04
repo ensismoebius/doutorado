48e67274782590f0ac66fc14a50a469015c07038
Andre Furlan <ensismoebius@gmail.com>
2022-09-04 17:37:25 -0300

    Resetting all code for a fresh start

--- src/lib/neuralNetwork/layer/Dense.cpp
+++
@@ -1,9 +0,0 @@
-#include "Dense.h"
-
-NeuralNetwork::Layers::Dense::Dense(unsigned int units, unsigned int shape[])
-{
-}
-
-NeuralNetwork::Layers::Dense::~Dense()
-{
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
-#include "lib/neuralNetwork/model/Sequential.h"
-#include "lib/neuralNetwork/layer/Dense.h"
+#include <Eigen/Core>
 
 int main(int argc, char const *argv[])
 {
-    NeuralNetwork::Layers::Dense l0(1, {1});
-
-    NeuralNetwork::Model::Sequential model({l0});
-
     return 0;
 }
