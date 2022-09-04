010e0d277bd74fb9d2e05fcf180b077f0701e2cf
Andre Furlan <ensismoebius@gmail.com>
2022-09-04 19:43:50 -0300

    Ramdon numbber generator

--- src/lib/neuralNetwork/utils/Rng.cpp
+++ src/lib/neuralNetwork/utils/Rng.cpp
@@ -1,8 +1,41 @@
 #include "Rng.h"
-Rng::Rng()
+Rng::Rng(unsigned long seed):
+    rand(seed ? (seed & max) : 1),
+    multiplier(16807),
+    max(2147483647L)
 {
 }
- 
-Rng::~Rng()
+
+void Rng::seed(const unsigned long seed) {
+    rand = (seed ? (seed & max) : 1);
+}
+
+double Rng::random() {
+    rand = nextRandonLong(rand);
+    return double(rand) / double(max);
+}
+
+inline long Rng::nextRandonLong(long seed)
 {
+    unsigned long lo, hi;
+
+    lo = multiplier * (long)(seed & 0xffff);
+    hi = multiplier * (long)((unsigned long)seed >> 16);
+    lo += (hi & 0x7fff) << 16;
+
+    if (lo > max)
+    {
+        lo &= max;
+        ++lo;
+    }
+
+    lo += hi >> 15;
+
+    if (lo > max)
+    {
+        lo &= max;
+        ++lo;
+    }
+
+    return (long)lo;
 }
--- src/lib/neuralNetwork/utils/Rng.h
+++ src/lib/neuralNetwork/utils/Rng.h
@@ -3,8 +3,14 @@
 class Rng
 {
 private:
-    
+    const unsigned int multiplier;
+    const unsigned long max;
+    long rand;
+
+    inline long nextRandonLong(long seed);
+
 public:
-    Rng();
-    ~Rng();
+    Rng(unsigned long seed);
+    virtual void seed(const unsigned long seed);
+    virtual double random();
 };
