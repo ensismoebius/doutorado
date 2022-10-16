123115e3c951e8c0cd30cfd4b05fea5bec1aef4d
André Furlan <ensismoebius@gmail.com>
2022-10-16 17:01:16 -0300

    Finally PROBLEM SOLVED for using an external library
    Finally PROBLEM SOLVED for using an external library
    which uses CMake as building method!

--- software/src/experiments/experiment01.cpp
+++ software/src/experiments/experiment01.cpp
@@ -1,37 +1,39 @@
 #include <armadillo>
+#include<Heatmap.h>
 
 #define m arma::Mat<float>
 
-m linreg(m x, m w, m b)
-{
-    return x * w + b;
-}
+//m linreg(m x, m w, m b)
+//{
+//    return x * w + b;
+//}
 
-void initializesWeights(m weights)
-{
-    weights.randu(weights.n_rows, weights.n_cols);
-}
+//void initializesWeights(m weights)
+//{
+//    weights.randu(weights.n_rows, weights.n_cols);
+//}
 
-// Generate y = X w + b + noise
-m syntheticData(m w, float b, int numExamples)
-{
-    m X(numExamples, w.n_rows);
+//// Generate y = X w + b + noise
+//m syntheticData(m w, float b, int numExamples)
+//{
+//    m X(numExamples, w.n_rows);
 
-    m y = linreg(X, w, {b});
+//    m y = linreg(X, w, {b});
 
-    // Add noise
-    m noise(y.n_rows, y.n_cols);
-    noise.randu(y.n_rows, y.n_cols);
+//    // Add noise
+//    m noise(y.n_rows, y.n_cols);
+//    noise.randu(y.n_rows, y.n_cols);
 
-    y += noise;
+//    y += noise;
 
-    return y;
-}
+//    return y;
+//}
 
 void experiment01()
 {
-    m w(1000, 2);
-    m trueW({2, -3.4f});
-    float trueB = 4.2f;
-    m y = syntheticData(trueW, trueB, 1000);
+//    m w(1000, 2);
+//    m trueW({2, -3.4f});
+//    float trueB = 4.2f;
+//    m y = syntheticData(trueW, trueB, 1000);
+//    sfplot::Heatmap
 }
