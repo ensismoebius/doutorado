62f2912cd64d360729ea42e8f1f603c15a117f4f
André Furlan <ensismoebius@gmai.com>
2025-09-26 02:40:32 -0300

    Minor fixes

--- software/nn/PackageChecking.cmake
+++ software/nn/PackageChecking.cmake
@@ -2,7 +2,7 @@
 find_package(PkgConfig REQUIRED)
 # Find Eigen
 
-find_package(Eigen3 3.3 REQUIRED NO_MODULE)
+find_package(Eigen3 REQUIRED NO_MODULE)
 
 # Find OpenMP
 find_package(OpenMP REQUIRED)
--- software/nn/src/util/synthetic_spike_data.hpp
+++ software/nn/src/util/synthetic_spike_data.hpp
@@ -23,4 +23,14 @@ using std::vector;
 auto generate_autoencoder_spike_data(int n_samples, int input_dim, int n_steps, float max_rate,
                                      float timeStep) -> tuple<vector<Tensor>, vector<Tensor>>;
 
+/**
+ * @brief Generates synthetic spike train data of ones.
+ * @param n_samples Number of samples.
+ * @param input_dim Number of input features.
+ * @param n_steps Number of time steps for spike train.
+ * @return std::vector<Eigen::MatrixXf> Vector of spike trains (one per time step).
+ */
+auto generate_autoencoder_spike_data_of_ones(int n_samples, int input_dim, int n_steps)
+    -> tuple<vector<Tensor>, vector<Tensor>>;
+
 #endif // SYNTHETIC_SPIKE_DATA_HPP
