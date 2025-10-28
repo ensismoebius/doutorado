8d35a238732b224d5fc821bde83430307f7f187d
André Furlan <ensismoebius@gmai.com>
2025-10-28 13:54:46 -0300

    Lets start the main program

---
+++ software/nn/src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ *  Main entry point for the monografia C++ project
+ *  This project implements various neural network architectures and training
+ * pipelines.
+ */
+#include <iostream>
+
+using std::cout;
+
+auto main() -> int {
+  cout << "Monografia C++ Project" << '\n';
+  cout << "Based on the dissertation: 'Autenticação Biométrica de Locutores "
+          "Drasticamente Disfônicos Aprimorada pela Imagined Speech'"
+       << '\n';
+
+  return 0;
+}
