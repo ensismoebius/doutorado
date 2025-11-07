e9b385aaf364af13929eed3809f4df2419b81255
André Furlan <ensismoebius@gmail.com>
2025-11-07 15:00:14 -0300

    More fixes

--- software/nn/src/statistics/statistics.cpp
+++ software/nn/src/statistics/statistics.cpp
@@ -8,61 +8,62 @@
  *
  */
 #include <cmath>
+#include <utility>
 #include <vector>
 
 namespace statistics
 {
-	double variance(std::vector<double> data)
-	{
-		double mean = 0;
-		double variance = 0;
+auto variance(std::vector<double> data) -> double
+{
+    double mean = 0;
+    double variance = 0;
 
-		// Calculate the mean
-		for (auto accuracy : data)
-		{
-			mean += accuracy;
-		}
-		mean /= data.size();
+    // Calculate the mean
+    for (auto accuracy : data)
+    {
+        mean += accuracy;
+    }
+    mean /= static_cast<double>(data.size());
 
-		// Calculate the variance
-		for (auto accuracy : data)
-		{
-			variance += std::pow(accuracy - mean, 2);
-		}
-		variance /= data.size();
+    // Calculate the variance
+    for (auto accuracy : data)
+    {
+        variance += std::pow(accuracy - mean, 2);
+    }
+    variance /= data.size();
 
-		return variance;
-	}
-
-	double variance(double *data, unsigned int length)
-	{
-		double mean = 0;
-		double variance = 0;
-
-		// Calculate the mean
-		for (unsigned int i = 0; i < length; i++)
-		{
-			mean += data[i];
-		}
-		mean /= length;
-
-		// Calculate the variance
-		for (unsigned int i = 0; i < length; i++)
-		{
-			variance += std::pow(data[i] - mean, 2);
-		}
-		variance /= length;
-
-		return variance;
-	}
-
-	double standardDeviation(std::vector<double> data)
-	{
-		return std::sqrt(variance(data));
-	}
-
-	double standardDeviation(double *data, unsigned int length)
-	{
-		return std::sqrt(variance(data, length));
-	}
+    return variance;
 }
+
+auto variance(double* data, unsigned int length) -> double
+{
+    double mean = 0;
+    double variance = 0;
+
+    // Calculate the mean
+    for (unsigned int i = 0; i < length; i++)
+    {
+        mean += data[i];
+    }
+    mean /= length;
+
+    // Calculate the variance
+    for (unsigned int i = 0; i < length; i++)
+    {
+        variance += std::pow(data[i] - mean, 2);
+    }
+    variance /= length;
+
+    return variance;
+}
+
+auto standardDeviation(std::vector<double> data) -> double
+{
+    return std::sqrt(variance(std::move(data)));
+}
+
+auto standardDeviation(double* data, unsigned int length) -> double
+{
+    return std::sqrt(variance(data, length));
+}
+} // namespace statistics
