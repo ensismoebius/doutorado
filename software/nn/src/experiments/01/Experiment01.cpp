e1af880c7b5e2d18e9d8027d203f3601cddc55e4
André Furlan <ensismoebius@gmail.com>
2025-11-10 11:59:02 -0300

    Fixed a bug in compiling

--- software/nn/src/experiments/01/Experiment01.cpp
+++ software/nn/src/experiments/01/Experiment01.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 
+#include "dataLoaders/AudioLoader.h"
 
 static void init()
 {
@@ -8,11 +10,22 @@ static void init()
 static void perform()
 {
     // Experiment 01 perform code goes here
+    auto [audioSamples, eegIndex] = nn::dataLoaders::loadAudioFromMat(
+        "/home/ensismoebius/Documentos/UNESP/doutorado/databases/BaseDeDatosHablaImaginada/S01/"
+        "S01_Audio.mat",
+        0);
+
+    // Use audioSamples and eegIndex as needed
+    (void) audioSamples;
+    (void) eegIndex;
+    // For example, print the EEG index
+    std::cout << "EEG Index: " << eegIndex << '\n';
 }
 
 auto main(int argc, char** argv) -> int
 {
     init();
     // Experiment 01 code goes here
+    perform();
     return 0;
 }
