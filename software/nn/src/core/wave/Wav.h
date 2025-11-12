eeaa4a336f9df93376e5088efb5cb88ffeca2bd2
André Furlan <ensismoebius@gmail.com>
2025-11-12 12:17:25 -0300

    Wav remake

--- software/nn/src/core/wave/Wav.h
+++ software/nn/src/core/wave/Wav.h
@@ -14,114 +14,116 @@
 
 #include <cstdint>
 #include <fstream>
-#include <iostream>
-#include <stdexcept>
 #include <string>
 #include <vector>
 
 class Wav
 {
-	private:
-		// signal properties
-		struct
-		{
-				/* RIFF Chunk Descriptor */
-				std::uint8_t RIFF[4];        // RIFF Header Magic header
-				std::uint32_t chunkSize;      // RIFF Chunk Size
-				std::uint8_t WAVE[4];        // WAVE Header
+   private:
+    // signal properties
+    struct
+    {
+        /* RIFF Chunk Descriptor */
+        std::uint8_t RIFF[4];    // RIFF Header Magic header
+        std::uint32_t chunkSize; // RIFF Chunk Size
+        std::uint8_t WAVE[4];    // WAVE Header
 
-				/* "fmt" sub-chunk */
-				std::uint8_t fmt[4];         // FMT header
-				std::uint32_t subchunk1Size;  // Size of the fmt chunk
-				std::uint16_t audioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
-				std::uint16_t numOfChan;      // Number of channels 1=Mono 2=Sterio
-				std::uint32_t samplingrate;  // Sampling Frequency in Hz
-				std::uint32_t bytesPerSec;    // bytes per second
-				std::uint16_t blockAlign;     // 2=16-bit mono, 4=16-bit stereo
-				std::uint16_t bitsPerSample;  // Number of bits per sample
+        /* "fmt" sub-chunk */
+        std::uint8_t fmt[4];         // FMT header
+        std::uint32_t subchunk1Size; // Size of the fmt chunk
+        std::uint16_t audioFormat;   // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM
+                                     // A-Law, 259=ADPCM
+        std::uint16_t numOfChan;     // Number of channels 1=Mono 2=Sterio
+        std::uint32_t samplingrate;  // Sampling Frequency in Hz
+        std::uint32_t bytesPerSec;   // bytes per second
+        std::uint16_t blockAlign;    // 2=16-bit mono, 4=16-bit stereo
+        std::uint16_t bitsPerSample; // Number of bits per sample
 
-				/* "data" sub-chunk */
-				std::uint8_t subchunk2ID[4]; // "data"  string
-				std::uint32_t subchunk2Size;  // Sampled data length
-		} headers;
+        /* "data" sub-chunk */
+        std::uint8_t subchunk2ID[4]; // "data"  string
+        std::uint32_t subchunk2Size; // Sampled data length
+    } headers;
 
-		// another signal properties
-		int amountOfData;
-		int waveResolution;
+    // another signal properties
+    int amountOfData;
+    int waveResolution;
 
-		// signal data
-		std::vector<long double> data;
-		std::vector<long double> dataLeft;
-		std::vector<long double> dataRight;
+    // signal data
+    std::vector<long double> data;
+    std::vector<long double> dataLeft;
+    std::vector<long double> dataRight;
 
-		// path of file containing the signal
-		std::string path;
+    // path of file containing the signal
+    std::string path;
 
-		// callback function (applied on data)
-		void (*callbackFunction)(std::vector<long double> &signal, int &signalLength, unsigned int samplingRate, std::string path);
+    // callback function (applied on data)
+    void (*callbackFunction)(std::vector<long double>& signal, int& signalLength,
+                             unsigned int samplingRate, std::string path);
 
-	public:
-		/**
-		 * Process the data
-		 */
-		void process();
+   public:
+    /**
+     * Process the data
+     */
+    void process();
 
-		/**
-		 * Read the wav file
-		 * @param path
-		 */
-		void read(std::string path);
+    /**
+     * Read the wav file
+     * @param path
+     */
+    void read(const std::string& _path);
 
-		/**
-		 * Write a wav file
-		 * @param path
-		 */
-		void write(std::string path);
+    /**
+     * Write a wav file
+     * @param path
+     */
+    void write(const std::string& _path);
 
-		/**
-		 * Returns the path of file containing the signal
-		 * @return path
-		 */
-		std::string getPath() const;
+    /**
+     * Returns the path of file containing the signal
+     * @return path
+     */
+    [[nodiscard]] auto getPath() const -> std::string;
 
-		/**
-		 * Returns the raw wav data (monolitic wav)
-		 * @return data
-		 */
-		std::vector<long double> getData() const;
+    /**
+     * Returns the raw wav data (monolitic wav)
+     * @return data
+     */
+    [[nodiscard]] auto getData() const -> std::vector<long double>;
 
-		/**
-		 * Returns the raw wav data (left channel wav)
-		 * @return data
-		 */
-		std::vector<long double> getDataLeft() const;
+    /**
+     * Returns the raw wav data (left channel wav)
+     * @return data
+     */
+    [[nodiscard]] auto getDataLeft() const -> std::vector<long double>;
 
-		/**
-		 * Returns the raw wav data (right channel wav)
-		 * @return data
-		 */
-		std::vector<long double> getDataRight() const;
+    /**
+     * Returns the raw wav data (right channel wav)
+     * @return data
+     */
+    [[nodiscard]] auto getDataRight() const -> std::vector<long double>;
 
-		/**
-		 * The function witch will manipulate the wav data
-		 * @param callbackFunction
-		 */
-		void setCallbackFunction(void (*callbackFunction)(std::vector<long double> &signal, int &signalLength, unsigned int samplingRate, std::string path));
+    /**
+     * The function witch will manipulate the wav data
+     * @param callbackFunction
+     */
+    void setCallbackFunction(void (*_callbackFunction)(std::vector<long double>& signal,
+                                                       int& signalLength, unsigned int samplingRate,
+                                                       std::string path));
 
-	private:
-		short convert2of8to1of16(unsigned char lsb, unsigned char msb);
-		void convert1of16to2of8(short result, unsigned char *lsb, unsigned char *msb);
-		void readWaveData(std::ifstream &ifs);
-		void readWaveHeaders(std::ifstream &ifs);
-		inline void write8Res1Channel(std::ofstream &ofs);
-		inline void write8Res2Channel(std::ofstream &ofs);
-		inline void write16Res1Channel(std::ofstream &ofs);
-		inline void write16Res2Channel(std::ofstream &ofs);
-		inline void read8Res1Channel(std::ifstream &ifs);
-		inline void read8Res2Channel(std::ifstream &ifs);
-		inline void read16Res1Channel(std::ifstream &ifs);
-		inline void read16Res2Channel(std::ifstream &ifs);
-		void clearVectors();
-		void resetMetaData();
+   private:
+    auto convert2of8to1of16(unsigned char lsb, unsigned char msb) -> short;
+    void convert1of16to2of8(short result, unsigned char* lsb, unsigned char* msb);
+    void readWaveData(std::ifstream& ifs);
+    void readWaveHeaders(std::ifstream& ifs);
+    inline void write8Res1Channel(std::ofstream& ofs);
+    inline void write8Res2Channel(std::ofstream& ofs);
+    inline void write16Res1Channel(std::ofstream& ofs);
+    inline void write16Res2Channel(std::ofstream& ofs);
+    inline void read8Res1Channel(std::ifstream& ifs);
+    inline void read8Res2Channel(std::ifstream& ifs);
+    inline void read16Res1Channel(std::ifstream& ifs);
+    inline void read16Res2Channel(std::ifstream& ifs);
+    void clearVectors();
+    void resetMetaData();
 };
 #endif /* SRC_LIB_WAV_H_ */
