efe2075f29d80612c60f64e778cb23304b63cc11
André Furlan <ensismoebius@gmail.com>
2025-12-09 16:12:51 -0300

    A little better CMake

--- software/nn/cmake/VendorIncludes.cmake
+++ software/nn/cmake/VendorIncludes.cmake
@@ -1,20 +1,5 @@
-# Sets up GoogleTest (downloads, configures, exposes GTest::GTest and GTest::Main)
-include(cmake/VendorGTest.cmake)
-
 # Cnpy integration
 include(cmake/VendorCnpy.cmake)
-
-# ImGui integration
-include(cmake/VendorImgui.cmake)
-
-# Implot integration
-include(cmake/VendorImplot.cmake)
-
-# Eigen parallelization settings
-include(cmake/VendorEigenParallel.cmake)
-
-# Vendored matio handling (sets MATIO_ROOT_DIR, MATIO_INCLUDE_DIR, MATIO_LIBRARY, exposes MATIO::MATIO)
-include(cmake/VendorMatio.cmake)
 
 # Vendored FFTW handling
 include(cmake/VendorFFTW.cmake)
@@ -22,5 +7,25 @@ include(cmake/VendorFFTW.cmake)
 # Vendored NFFT3 handling
 include(cmake/VendorNFFT3.cmake)
 
-# Workarounds / shims for matio-cpp configure/export behavior
+# ImGui integration
+include(cmake/VendorImgui.cmake)
+
+# Sets up GoogleTest (downloads, 
+# configures, exposes GTest::GTest 
+# and GTest::Main)
+include(cmake/VendorGTest.cmake)
+
+# Vendored matio handling (sets 
+# MATIO_ROOT_DIR, MATIO_INCLUDE_DIR, 
+# MATIO_LIBRARY, exposes MATIO::MATIO)
+include(cmake/VendorMatio.cmake)
+
+# Implot integration
+include(cmake/VendorImplot.cmake)
+
+# Workarounds / shims for matio-cpp 
+# configure/export behavior
 include(cmake/VendorMatioCppShim.cmake)
+
+# Eigen parallelization settings
+include(cmake/VendorEigenParallel.cmake)
