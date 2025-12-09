7c4c5185551704a0541502d646b83c0c649fd5b4
André Furlan <ensismoebius@gmail.com>
2025-12-09 13:22:12 -0300

    nfft fft libs

---
+++ software/nn/cmake/VendorFFTW.cmake
@@ -0,0 +1,30 @@
+# VendorFFTW.cmake
+# Configure vendored lib/fftw presence and make FFTW::FFTW available
+
+include(FetchContent)
+
+FetchContent_Declare(
+    fftw
+    URL            https://fftw.org/fftw-3.3.10.tar.gz
+    URL_HASH       SHA256=56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+)
+
+# Disable OpenMP in FFTW to avoid clang OpenMP detection issues
+set(ENABLE_OPENMP OFF CACHE BOOL "Disable OpenMP in FFTW" FORCE)
+set(ENABLE_THREADS OFF CACHE BOOL "Disable THREADS in FFTW" FORCE)
+set(BUILD_TESTS OFF CACHE BOOL "Disable FFTW tests" FORCE)
+
+FetchContent_MakeAvailable(fftw)
+
+# target_include_directories(FFTW3::fftw3 SYSTEM
+#   PUBLIC
+#     "${fftw_SOURCE_DIR}/api"
+# )
+
+# # Provide a lightweight imported "fftw" target if the subproject doesn't
+# # create it early enough. This helps export/install steps in other vendored
+# # CMakeLists succeed.
+# if(TARGET FFTW3::fftw3)
+#   add_library(FFTW::FFTW ALIAS FFTW3::fftw3)
+# endif()
---
+++ software/nn/cmake/VendorNFFT3.cmake
@@ -0,0 +1,28 @@
+# VendorNFFT3.cmake
+# Configure vendored lib/nfft3 presence and make NFFT::NFFT available
+
+include(FetchContent)
+
+include(ExternalProject)
+
+ExternalProject_Add(nfft3
+    URL            https://github.com/NFFT/nfft/releases/download/3.5.3/nfft-3.5.3.tar.gz
+    URL_HASH       SHA256=caf1b3b3e5bf8c33a6bfd7eca811d954efce896605ecfd0144d47d0bebdf4371
+    DOWNLOAD_DIR   "${CMAKE_BINARY_DIR}/_deps/nfft3-src"
+    SOURCE_DIR     "${CMAKE_BINARY_DIR}/_deps/nfft3-src"
+    INSTALL_DIR    "${CMAKE_BINARY_DIR}/_deps/nfft3-install"
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+
+    PATCH_COMMAND
+        "bash" "-c" "cd <SOURCE_DIR> && ./bootstrap.sh"
+
+    CONFIGURE_COMMAND "bash" "-c" "cd <SOURCE_DIR> && ./configure --prefix=<INSTALL_DIR> --disable-examples --disable-applications --disable-openmp"
+    BUILD_COMMAND "bash" "-c" "cd <SOURCE_DIR> && make"
+    INSTALL_COMMAND "bash" "-c" "cd <SOURCE_DIR> && make install"
+)
+
+add_library(NFFT::NFFT UNKNOWN IMPORTED)
+set_target_properties(NFFT::NFFT PROPERTIES
+    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_deps/nfft3-install/lib/libnfft3.a" # Assuming static library
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/_deps/nfft3-install/include"
+)
