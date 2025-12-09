27abc70c63a11b523a2cdedee78e389cfa9400b9
André Furlan <ensismoebius@gmail.com>
2025-12-09 18:43:37 -0300

    More fixes

--- software/nn/cmake/VendorNFFT3.cmake
+++ software/nn/cmake/VendorNFFT3.cmake
@@ -8,37 +8,45 @@ include(ExternalProject)
 find_package(OpenMP REQUIRED)
 
 set(FFTW_INSTALL_DIR "${CMAKE_BINARY_DIR}/_deps/fftw-install")
+set(NFFT3_INSTALL_DIR "${CMAKE_BINARY_DIR}/_deps/nfft3-install")
+file(MAKE_DIRECTORY "${NFFT3_INSTALL_DIR}")
+
 
 ExternalProject_Add(nfft3
     URL            https://github.com/NFFT/nfft/releases/download/3.5.3/nfft-3.5.3.tar.gz
     URL_HASH       SHA256=caf1b3b3e5bf8c33a6bfd7eca811d954efce896605ecfd0144d47d0bebdf4371
-    DOWNLOAD_DIR   "${CMAKE_BINARY_DIR}/_deps/nfft3-src"
-    SOURCE_DIR     "${CMAKE_BINARY_DIR}/_deps/nfft3-src"
-    INSTALL_DIR    "${CMAKE_BINARY_DIR}/_deps/nfft3-install"
+    DOWNLOAD_DIR   "${CMAKE_BINARY_DIR}/_deps"
+    INSTALL_DIR    "${NFFT3_INSTALL_DIR}"
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    BUILD_IN_SOURCE TRUE
+
+    # Make Ninja/CMake aware of which files this ExternalProject will produce
+    BUILD_BYPRODUCTS "${NFFT3_INSTALL_DIR}/lib/libnfft3.so"
 
     # Ensure FFTW is built and installed before configuring NFFT3
     DEPENDS fftw_build
-
-    PATCH_COMMAND
-        "bash" "-c" "cd <SOURCE_DIR> && ./bootstrap.sh && autoreconf --install --force"
     
     CONFIGURE_COMMAND
-        "bash" "-c" "cd <SOURCE_DIR> && ./configure --prefix=<INSTALL_DIR> --disable-examples --disable-applications --enable-openmp --enable-shared --with-fftw3=${FFTW_INSTALL_DIR}"
+        <SOURCE>/configure --prefix=${NFFT3_INSTALL_DIR} --disable-examples --disable-applications --enable-openmp --enable-shared --with-fftw3=${FFTW_INSTALL_DIR}
 
     BUILD_COMMAND
-        "bash" "-c" "cd <SOURCE_DIR> && make -j4"
+        make -j4
 
     INSTALL_COMMAND
-        "bash" "-c" "cd <SOURCE_DIR> && make install"
+        make install
+
+    LOG_DOWNLOAD 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
 )
 
 add_library(NFFT::NFFT SHARED IMPORTED GLOBAL)
 
 set_target_properties(NFFT::NFFT
     PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/_deps/nfft3-install/lib/libnfft3.so"
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/_deps/nfft3-install/include"
+        IMPORTED_LOCATION "${NFFT3_INSTALL_DIR}/lib/libnfft3.so"
+        INTERFACE_INCLUDE_DIRECTORIES "${NFFT3_INSTALL_DIR}/include"
         INTERFACE_LINK_LIBRARIES "OpenMP::OpenMP_C"
 )
 
