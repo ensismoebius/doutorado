9428e1a8bb1cca30e7e117c5126fb68f9064d335
André Furlan <ensismoebius@gmail.com>
2026-01-05 08:07:27 -0300

    less warnnings

--- software/nn/cmake/VendorYaml.cmake
+++ software/nn/cmake/VendorYaml.cmake
@@ -3,6 +3,10 @@
 
 include(FetchContent)
 
+set(YAML_CPP_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
+set(YAML_CPP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(YAML_CPP_BUILD_CONTRIB OFF CACHE BOOL "" FORCE)
+
 FetchContent_Declare(
     yaml-cpp
     GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
